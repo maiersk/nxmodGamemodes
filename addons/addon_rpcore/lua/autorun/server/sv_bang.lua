745378eba1d9fc453da6409fba0d34c5c13ed73a
cyber space <1119508543@qq.com>
2018-11-27 23:37:23 +0800

    帮派原型搭建

---
+++ addons/addon_rpcore/lua/autorun/server/sv_bang.lua
@@ -0,0 +1,57 @@
+--帮派 服务器端
+
+local bang = {}
+
+--创建帮派
+function bang.create()
+
+end 
+
+--解散帮派
+function bang.jiesan()
+
+end 
+
+--将某人移出帮派
+function bang.yichu()
+
+
+end 
+
+--申请加入帮派
+function bang.shenqing()
+
+end 
+
+--退出帮派
+function bang.quit()
+
+end
+
+--邀请某人加入帮派
+function bang.yaoqing()
+
+end
+
+--任命某人职务
+function bang.renming()
+
+end
+
+--辞职
+function bang.cizhi()
+
+end
+
+--发起任务
+function bang.renwu()
+
+end 
+
+--占领地盘
+function bang.zhanling()
+
+end
+
+
+return bang
